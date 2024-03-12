287a5a69887eaf12127b6b5532a0ee38409e1244
svenaci <steffan_v@hotmail.com>
2024-03-12 12:36:55 -0400

    add comment

--- src/app/api/auth/[…nextauth] /route.ts
+++ src/app/api/auth/[…nextauth] /route.ts
@@ -3,6 +3,7 @@ import RedditProvider from "next-auth/providers/reddit";
 
 export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
+  //https://medium.com/@rezahedi/using-nextauth-authentication-provider-in-next-js-by-app-router-f50cb23282c9
   providers: [
     RedditProvider({
       clientId: process.env.REDDIT_CLIENT_ID,
