c7bea74387afff4e805f267658904a83c819dea0
svenaci <steffan_v@hotmail.com>
2024-03-14 18:08:12 -0400

    implement body subreddit and image for postbox component

--- src/app/components/PostBox.tsx
+++ src/app/components/PostBox.tsx
@@ -1,18 +1,35 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "./Avatar";
-
 import { LinkIcon, PhotoIcon } from "@heroicons/react/24/outline";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  postTitle: string;
+  postBody: string;
+  postImage: string;
+  subreddit: string;
+};
 
 function PostBox() {
   const { data: session } = useSession();
+  const [imageBoxOpen, setImageBoxOpen] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    setValue,
+    formState: { errors },
+  } = useForm<FormData>();
+
   return (
     <form className="sticky top-16 z-50 bg-white border border-gray-300 rounded-md p-2">
       <div className="flex items-center space-x-3">
         <Avatar />
         <input
+          {...register("postTitle", { required: true })}
           disabled={!session}
           className="rounded-md flex-1 bg-gray-50 p-2 pl-5 outline-none"
           type="text"
@@ -21,9 +38,49 @@ function PostBox() {
           }
         />
 
-        <PhotoIcon className={`h-6 text-gray-300 cursor-pointer`} />
+        <PhotoIcon
+          onClick={() => setImageBoxOpen(!imageBoxOpen)}
+          className={`h-6 text-gray-300 cursor-pointer ${
+            imageBoxOpen && "text-blue-300"
+          }`}
+        />
         <LinkIcon className="h-6 text-gray-300" />
       </div>
+
+      {!!watch("postTitle") && (
+        <div className="flex flex-col py-2">
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Body:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("postBody")}
+              type="text"
+              placeholder="text (optional)"
+            />
+          </div>
+          <div className="flex items-center px-2">
+            <p className="min-w-[90px]">Subreddit:</p>
+            <input
+              className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+              {...register("subreddit")}
+              type="text"
+              placeholder="i.e. reactjs"
+            />
+          </div>
+
+          {imageBoxOpen && (
+            <div className="flex items-center px-2">
+              <p className="min-w-[90px]">Image URL:</p>
+              <input
+                className="m-2 flex-1 bg-blue-50 p-2 outline-none"
+                {...register("postImage")}
+                type="text"
+                placeholder="Optional..."
+              />
+            </div>
+          )}
+        </div>
+      )}
     </form>
   );
 }
