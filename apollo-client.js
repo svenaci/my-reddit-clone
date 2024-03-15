5cb9cf05a8ea6dbccdc33fba6156c03ec5c0625f
svenaci <steffan_v@hotmail.com>
2024-03-15 11:19:50 -0400

    not return as function

--- apollo-client.js
+++ apollo-client.js
@@ -1,13 +1,11 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
-const createApolloClient = () => {
-  return new ApolloClient({
-    uri: "https://milhaud.stepzen.net/api/early-mouse/__graphql",
-    header: {
-      Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
-    },
-    cache: new InMemoryCache(),
-  });
-};
+const client = new ApolloClient({
+  uri: "https://milhaud.stepzen.net/api/early-mouse/__graphql",
+  headers: {
+    Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
-export default createApolloClient;
+export default client;
