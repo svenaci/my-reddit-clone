8eb61ed88a6855b1032dc00bff4fad5d91dbb7e9
svenaci <steffan_v@hotmail.com>
2024-03-16 15:07:29 -0400

    add spacing around posts in the feed

--- src/app/components/Feed.tsx
+++ src/app/components/Feed.tsx
@@ -11,7 +11,7 @@ function Feed() {
   const posts: Post[] = data?.postList;
 
   return (
-    <div>
+    <div className="mt-5 space-y-4">
       {posts?.map((post) => (
         <Post key={post.id} post={post} />
       ))}
