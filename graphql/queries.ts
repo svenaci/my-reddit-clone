c32e5695dedb6f03d84ac6bc0ee89d286912687b
svenaci <steffan_v@hotmail.com>
2024-03-18 11:12:11 -0400

    create query to retrieve all posts by topic

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -42,3 +42,36 @@ export const GET_POST_LIST = gql`
     }
   }
 `;
+
+export const GET_ALL_POSTS_BY_TOPIC = gql`
+  query MyQuery($topic: String!) {
+    postListByTopic(topic: $topic) {
+      body
+      created_at
+      id
+      image
+      title
+      subreddit_id
+      username
+      comments {
+        created_at
+        id
+        post_id
+        text
+        username
+      }
+      subreddit {
+        created_at
+        id
+        topic
+      }
+      votes {
+        created_at
+        id
+        post_id
+        upvote
+        username
+      }
+    }
+  }
+`;
