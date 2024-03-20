69aab880db34c1842e59a88a5c0294dfbd2741e5
svenaci <steffan_v@hotmail.com>
2024-03-20 09:58:37 -0400

    create ADD COMMENT mutation:

--- src/app/post/[postId]/page.tsx
+++ src/app/post/[postId]/page.tsx
@@ -32,17 +32,25 @@ function PostPage() {
     formState: { errors },
   } = useForm<FormData>();
 
+  const onSubmit: SubmitHandler<FormData> = async (data) => {
+    console.log("here");
+  };
+
   return (
     <div className="mx-auto my-7 max-w-5xl">
       <Post post={post} />
 
-      <div className="-mt-2 rounded-b-md border border-t-0 border-gray-300 bg-white p-5">
+      <div className="-mt-1 rounded-b-md border border-t-0 border-gray-300 bg-white p-5 pl-16">
         <p className="text-sm">
           Comment as <span>{session?.user?.name}</span>
         </p>
 
-        <form className="flex flex-col space-y-2">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2"
+        >
           <textarea
+            {...register("comment")}
             disabled={!session}
             className="h-24 rounded-md border border-gray-200 p-2 pl-4 outline-none disabled:bg-gray-50"
             placeholder={
